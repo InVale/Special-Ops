51ce45fe7e7c0c13158c60f46d3b34e87efbb8d0
Unknown <jean.auquier@live.fr>
2017-03-19 18:47:07 +0100

    True commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Player : MonoBehaviour {
+
+	public Camera MyCamera;
+	public UnitArray[] PlayerUnits;
+	public LayerMask HittableLayer;
+	public LayerMask GroundLayer;
+
+	[Serializable]
+	public struct UnitArray {
+		public KeyCode MyKey;
+		public Unit MyUnit;
+	}
+
+	void Update () {
+
+		foreach (UnitArray currentUnit in PlayerUnits) {
+			if (Input.GetKeyDown(currentUnit.MyKey)) {
+				Ray ray = MyCamera.ScreenPointToRay(Input.mousePosition);
+				RaycastHit hit;
+				if (Physics.Raycast(ray, out hit, Mathf.Infinity, HittableLayer)) {
+					if ( GroundLayer == (GroundLayer | (1 << hit.collider.gameObject.layer))) {
+						currentUnit.MyUnit.Destination(hit.point);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Unit : MonoBehaviour {
+
+	NavMeshAgent _myAgent;
+
+	void Start () {
+		_myAgent = gameObject.GetComponent<NavMeshAgent>();
+	}
+	
+	public void Destination (Vector3 destination) {
+		_myAgent.destination = destination;
+	}
+}
