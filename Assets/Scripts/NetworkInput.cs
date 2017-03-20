ba48aebbde3179c4a8068a2a6e72e3bcbdf8f540
Unknown <jean.auquier@live.fr>
2017-03-20 19:39:17 +0100

    Added very basic Network and a cute lil' FX

---
+++ Assets/Scripts/NetworkInput.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using DG.Tweening;
+
+[NetworkSettings (sendInterval = .03f, channel = 0)]
+public class NetworkInput : NetworkBehaviour
+{
+	[SyncVar]
+	public Vector3 realPosition = Vector3.zero;
+	[SyncVar]
+	public Vector3 realVelocity;
+	[SyncVar]
+	public Vector3 realRotation;
+	//		private float lastTime;
+
+	public float alpha;
+
+	void Update ()
+	{
+
+		//		alpha = 0.1f / (Time.time - lastTime) / .03f;
+
+		if (isLocalPlayer) {
+			realPosition = transform.position;
+			realVelocity = GetComponent<Rigidbody> ().velocity;
+			CmdSync (transform.position, GetComponent<Rigidbody> ().velocity, transform.eulerAngles);
+		} else {
+
+			transform.DOKill ();
+			transform.DOMove (realPosition, 0.1f, false);
+			transform.DORotate (realRotation, 0.1f);
+			GetComponent<Rigidbody> ().velocity = realVelocity;	
+		}
+	}
+
+	[Command]
+	void CmdSync (Vector3 position, Vector3 velocity, Vector3 rotation)
+	{
+		realPosition = position;
+		realVelocity = velocity;
+		realRotation = rotation;
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using System;
 
 public class Player : MonoBehaviour {
 
-	public Camera MyCamera;
+	public GameObject CirclePrefab;
 	public UnitArray[] PlayerUnits;
 	public LayerMask HittableLayer;
 	public LayerMask GroundLayer;
@@ -16,15 +16,22 @@ public class Player : MonoBehaviour {
 		public Unit MyUnit;
 	}
 
+	Camera myCamera;
+
+	void Start () {
+		myCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+	}
+
 	void Update () {
 
 		foreach (UnitArray currentUnit in PlayerUnits) {
 			if (Input.GetKeyDown(currentUnit.MyKey)) {
-				Ray ray = MyCamera.ScreenPointToRay(Input.mousePosition);
+				Ray ray = myCamera.ScreenPointToRay(Input.mousePosition);
 				RaycastHit hit;
 				if (Physics.Raycast(ray, out hit, Mathf.Infinity, HittableLayer)) {
 					if ( GroundLayer == (GroundLayer | (1 << hit.collider.gameObject.layer))) {
 						currentUnit.MyUnit.Destination(hit.point);
+						Instantiate(CirclePrefab, hit.point, Quaternion.identity);
 					}
 				}
 			}
