8d0b0c76645f126500455d7def569be446ee1a88
Unknown <jean.auquier@live.fr>
2017-03-23 22:05:25 +0100

    Spent some Time trying to understand Networking. Must do Character Spawn

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.Networking;
+using DG.Tweening;
+using UnityEngine.AI;
 
-public class Player : MonoBehaviour {
+[NetworkSettings (sendInterval = .03f, channel = 0)]
+public class Player : NetworkBehaviour {
 
 	public GameObject CirclePrefab;
 	public UnitArray[] PlayerUnits;
@@ -19,19 +23,23 @@ public class Player : MonoBehaviour {
 	Camera myCamera;
 
 	void Start () {
-		myCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+		if (isLocalPlayer) {
+			myCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+		}
 	}
 
 	void Update () {
 
-		foreach (UnitArray currentUnit in PlayerUnits) {
-			if (Input.GetKeyDown(currentUnit.MyKey)) {
-				Ray ray = myCamera.ScreenPointToRay(Input.mousePosition);
-				RaycastHit hit;
-				if (Physics.Raycast(ray, out hit, Mathf.Infinity, HittableLayer)) {
-					if ( GroundLayer == (GroundLayer | (1 << hit.collider.gameObject.layer))) {
-						currentUnit.MyUnit.Destination(hit.point);
-						Instantiate(CirclePrefab, hit.point, Quaternion.identity);
+		if (isLocalPlayer) {
+			foreach (UnitArray currentUnit in PlayerUnits) {
+				if (Input.GetKeyDown(currentUnit.MyKey)) {
+					Ray ray = myCamera.ScreenPointToRay(Input.mousePosition);
+					RaycastHit hit;
+					if (Physics.Raycast(ray, out hit, Mathf.Infinity, HittableLayer)) {
+						if ( GroundLayer == (GroundLayer | (1 << hit.collider.gameObject.layer))) {
+							currentUnit.MyUnit.Destination(hit.point);
+							Instantiate(CirclePrefab, hit.point, Quaternion.identity);
+						}
 					}
 				}
 			}
